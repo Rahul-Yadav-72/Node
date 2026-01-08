6e49f3190f5f086354c8b51ce570a410377d3ab1
Rahul Kumar Yadav <ry642499@gmail.com>
2026-01-08 10:35:38 +0530

    Initial commit: added Node.js practice files and API project

---
+++ first.js
@@ -0,0 +1,38 @@
+console.log("Rahul Yadav");
+console.log("Yadav ji ")
+
+
+var a = 20
+
+a
+
+let b = 30
+
+const c= 40 
+
+
+a = 30
+
+b = 45
+
+c = 100
+
+typeof a
+
+a+b
+
+a = 'Rku'
+
+typeof a
+
+typeof b
+
+a+b
+
+a*b
+
+isLogin = true
+
+typeof isLogin
+
+my 
---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log("Hello there!!");
+
+// function myFun(fname, lname) {
+//     console.log(fname + " " + lname);
+//     console.log(`Welcome ${fname} ${lname}`);
+// }
+
+// // myFun("rahul", "yadav");
+
+// // Function to perform all arithmetic operations
+// function sumTwoNum(num1, num2) {
+//     console.log(`Sum : ${num1 + num2}`);
+//     console.log(`Sub : ${num1 - num2}`);
+//     console.log(`Mul : ${num1 * num2}`);
+//     console.log(`Div : ${num1 / num2}`);
+// }
+
+// sumTwoNum(2, 3);
+
+
+const mynum = n => n%2 == 0 ? 2*n : 3*n;
+console.log(mynum(4));
+console.log(mynum(44));
+console.log(mynum(6));
