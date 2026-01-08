aa1f6446e31c17c158626770e37f8d9c17d843ff
Rahul Kumar Yadav <ry642499@gmail.com>
2026-01-08 11:40:53 +0530

    Update the index.js file .j

--- MY_API/index.js
+++ MY_API/index.js
@@ -3,4 +3,30 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', (req, res) => res.send('From post'))
+app.put('/',(req,res) => res.send('From Put '))
+app.delete('/',(req,res) => res.send('From Delete '))
+app.patch('/',(req,res) => res.send('From Pach'))
+app.head('/',(req,res) => res.send('From Pach'))
+app.get('/uni', (req, res) => res.send('Welcome to RKU!!'))
+app.get('/student', (req, res) => res.send({
+
+    name:"Rahul Kumar Yadav",
+    city:"Siwan",
+    sem: 6,
+    div: 'c',
+    branch: 'CE'
+
+}
+))
+app.get('/example/b', (req, res, next) => {
+    console.log('the response will be sent by the next function ...')
+    next()
+  }, (req, res) => {
+    res.send('Hello from B!')
+  })
+  
+app.get('/myname/:fname/:lname',(req,res) => res.send(
+    `Welcome ${req.params.fname} ${req.params.lname}`
+))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
